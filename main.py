af1accade8c67df6a1d8c15ca57cf47c2b342baf
IoIxD <alphaproject217@gmail.com>
2022-12-13 19:52:18 -0700

    more async friendly

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import discord
+import discord, threading, asyncio
 
-client = discord.Client(
-	intents=discord.Intents(
-		message_content=True,
-		guild_messages=True,
-		guilds=True,
-	)
-)
 
 bots_only = False
 
-@client.event
-async def on_ready():
+async def main_bot(client):
 	guild_id = -1
 	channel_id = -1
-
 	# ask for the guild id
 	while guild_id <= -1:
 		try:
@@ -41,11 +32,10 @@ async def on_ready():
 	# ask what file to write to
 	file_name = input("What file should this be saved to? .json will automatically be appended to whatever you enter:\n")
 
-	await archive_guild(guild_id,channel_id,file_name+".json")
+	await archive_guild(client,guild_id,channel_id,file_name+".json")
 	await client.close()
 
-@client.event
-async def archive_guild(guild_id,channel_id,file_name):
+async def archive_guild(client,guild_id,channel_id,file_name):
 	# clear the file
 	f = open(file_name,'w')
 	f.write("")
@@ -54,12 +44,11 @@ async def archive_guild(guild_id,channel_id,file_name):
 	if(channel_id == -1):
 		for channel in guild.channels:
 			if(channel.type == discord.ChannelType.text):
-				await archive_channel(channel,file_name)
+				await archive_channel(client,channel,file_name)
 	else:
-		await archive_channel(guild.get_channel(channel_id),file_name)
+		await archive_channel(client,guild.get_channel(channel_id),file_name)
 
-@client.event
-async def archive_channel(channel,file_name):
+async def archive_channel(client,channel,file_name):
 	# start 
 	f = open(file_name,'a')
 	try:
@@ -74,6 +63,37 @@ async def archive_channel(channel,file_name):
 		print("Channel "+channel.name+" is locked from the bot.")
 		pass
 	f.close()
+
+def run_async(func, *args, **kwargs):
+	try:
+		loop = asyncio.get_running_loop()
+	except RuntimeError:
+		loop = None
+	if loop is not None and loop.is_running():
+		thread = RunThread(func, args, kwargs)
+		thread.start()
+		thread.join()
+		return thread.result
+	else:
+		loop = asyncio.new_event_loop()
+		asyncio.set_event_loop(loop)
+		try:
+			loop.run_until_complete(func(*args, **kwargs))
+		except RuntimeError:
+			print("runtime error")
+		loop.close()
+
+client = discord.Client(
+	intents=discord.Intents(
+		message_content=True,
+		guild_messages=True,
+		guilds=True,
+	)
+)
+
+@client.event
+async def on_ready():
+	await main_bot(client)
 
 f = open("key","r")
 if f is None:
